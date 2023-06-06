4980c1552721c2ed91b69da5d23269136de1675c
harniy <forbedlam@gmail.com>
2023-06-06 14:50:35 +0300

    fix: favicon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import faviconDark from '@/assets/img/favicon/favicon-dark.png'
+import faviconlight from '@/assets/img/favicon/favicon-light.png'
 import './assets/styles/main.scss'
+
+const { state } = useQuasarTheme()
 
 useHead({
   title: import.meta.env.VITE_APP_TITLE,
   meta: [
     { name: 'description', content: import.meta.env.VITE_APP_DESCRIPTION },
     { name: 'keywords', content: import.meta.env.VITE_APP_KEYWORDS },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => state.isDark ? faviconlight : faviconDark),
+    },
   ],
 })
 
