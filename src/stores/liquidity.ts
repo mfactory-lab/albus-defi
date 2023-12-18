db2c58e912bc64fc444d22f8cf8244e347ee58f0
MGrgr <grkapran@gmail.com>
2023-12-18 21:13:51 +0300

    fix(liquidity): improve deposit single

--- src/stores/liquidity.ts
+++ src/stores/liquidity.ts
@@ -175,37 +175,45 @@ export const useLiquidityStore = defineStore('liquidity', () => {
       console.log('TokenSwap is not initialized...')
       return
     }
+    try {
+      state.swapping = true
 
-    const userSourceMint = swapState.value.direction === SwapDirection.ASC ? tokenSwap.value.data.tokenAMint : tokenSwap.value.data.tokenBMint
-    const userSource = await getAssociatedTokenAddress(userSourceMint, wallet.value!.publicKey)
-    const userDestination = await getAssociatedTokenAddress(tokenSwap.value.data.poolMint, wallet.value!.publicKey)
+      const userSourceMint = swapState.value.direction === SwapDirection.ASC ? tokenSwap.value.data.tokenAMint : tokenSwap.value.data.tokenBMint
+      const userSource = await getAssociatedTokenAddress(userSourceMint, wallet.value!.publicKey)
+      const userDestination = await getAssociatedTokenAddress(tokenSwap.value.data.poolMint, wallet.value!.publicKey)
 
-    const sourceTokenAmount = Number(solToLamports(state.amountTokenA ?? 0, swapState.value.from.decimals))
+      const sourceTokenAmount = Number(solToLamports(state.amountTokenA ?? 0, swapState.value.from.decimals))
 
-    const signature = await swapClient.value.depositSingleTokenTypeExactAmountIn({
-      tokenSwap: tokenSwap.value?.pubkey,
-      poolMint: tokenSwap.value.data.poolMint,
-      source: userSource,
-      destination: userDestination,
-      swapTokenA: tokenSwap.value.data.tokenA,
-      swapTokenB: tokenSwap.value.data.tokenB,
-      sourceTokenAmount,
-      minimumPoolTokenAmount: 0,
-    })
+      const signature = await swapClient.value.depositSingleTokenTypeExactAmountIn({
+        tokenSwap: tokenSwap.value?.pubkey,
+        poolMint: tokenSwap.value.data.poolMint,
+        sourceTokenMint: userSourceMint,
+        source: userSource,
+        destination: userDestination,
+        swapTokenA: tokenSwap.value.data.tokenA,
+        swapTokenB: tokenSwap.value.data.tokenB,
+        sourceTokenAmount,
+        minimumPoolTokenAmount: 0,
+      })
 
-    reload()
+      reload()
 
-    notify({
-      message: 'Transaction confirmed',
-      type: 'positive',
-      actions: [{
-        label: 'Explore',
-        color: 'white',
-        target: '_blank',
-        href: `https://explorer.solana.com/tx/${signature}?cluster=${connectionStore.cluster}`,
-        onClick: () => false,
-      }],
-    })
+      notify({
+        message: 'Transaction confirmed',
+        type: 'positive',
+        actions: [{
+          label: 'Explore',
+          color: 'white',
+          target: '_blank',
+          href: `https://explorer.solana.com/tx/${signature}?cluster=${connectionStore.cluster}`,
+          onClick: () => false,
+        }],
+      })
+    } catch (e) {
+      console.log(e)
+    } finally {
+      state.swapping = false
+    }
   }
 
   function openSlippage() {
