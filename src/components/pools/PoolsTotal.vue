8e95e71295d2d6ba433f160798ba4f7265b76cad
MGrgr <grkapran@gmail.com>
2024-02-01 20:53:51 +0300

    fix: pools page loader

--- src/components/pools/PoolsTotal.vue
+++ src/components/pools/PoolsTotal.vue
@@ -3,11 +3,18 @@ import { formatBalance } from '@/utils'
 
 const poolsStatsStore = usePoolsStatsStore()
 const poolsStatsTotal = computed(() => poolsStatsStore.poolsStatsTotal)
+const poolsLoading = computed(() => poolsStatsStore.poolsLoading)
+const txLoading = computed(() => poolsStatsStore.txLoading)
+const priceLoading = computed(() => poolsStatsStore.priceLoading)
 </script>
 
 <template>
   <div class="full-width q-pl-md">
-    <q-inner-loading :showing="poolsStatsTotal.tvl === 0" class="swap-loading" color="grey" />
+    <q-inner-loading
+      :showing="poolsStatsTotal.tvl === 0 && (poolsLoading || txLoading || priceLoading)"
+      class="swap-loading"
+      color="grey"
+    />
     <div class="row justify-start text-subtitle1 q-pl-xs">
       <span class="q-mr-md">TVL: ${{ formatBalance(poolsStatsTotal.tvl, 2) }}</span>
       <span>Volume 24h: ${{ formatBalance(poolsStatsTotal.volume24, 2) }}</span>
