1058dbc7be178544c23ef219eb97cd48bebb5faa
harniy <forbedlam@gmail.com>
2024-02-29 15:24:12 +0200

    fix: add pubkey to certificate redirect

--- src/hooks/certificate.ts
+++ src/hooks/certificate.ts
@@ -1,10 +1,13 @@
 import { AlbusClientEnv, ProofRequestStatus } from '@albus-finance/sdk'
+import { useWallet } from 'solana-wallets-vue'
 import { ALBUS_APP_URL, ENVIRONMENT } from '@/config'
 
 export function useCertificate(policy?: string) {
   const connectionStore = useConnectionStore()
   const cluster = computed(() => connectionStore.cluster)
   const route = useRoute()
+
+  const wallet = useWallet()
 
   const userStore = useUserStore()
 
@@ -25,16 +28,17 @@ export function useCertificate(policy?: string) {
   })
 
   const certificateLink = computed(() => {
+    const pubkey = wallet.publicKey.value?.toBase58()
     // TODO: remove temporary solution with albusUrl variable
     let albusUrl = ALBUS_APP_URL
     if (ENVIRONMENT !== AlbusClientEnv.PROD && cluster.value === 'mainnet-beta') {
       albusUrl = 'https://stage.app.albus.finance'
     }
     if (certificate.value && certificate.value.data?.status !== ProofRequestStatus.Rejected && !certificateExpired.value) {
-      return `${albusUrl}/holder?certificate=${certificate.value.pubkey?.toBase58()}`
+      return `${albusUrl}/holder?certificate=${certificate.value.pubkey?.toBase58()}&cluster=${cluster.value}${pubkey ? `&pubkey=${pubkey}` : ''}`
     }
     const redirect = encodeURIComponent(`${location.origin}${route.fullPath}`)
-    return `${albusUrl}/wizard/${requiredPolicy.value}?cluster=${cluster.value}&redirect=${redirect}`
+    return `${albusUrl}/wizard/${requiredPolicy.value}?cluster=${cluster.value}&redirect=${redirect}${pubkey ? `&pubkey=${pubkey}` : ''}`
   })
 
   return {
